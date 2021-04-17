01bb29bf563807189f4289e588b9c39cd2138d01
Gan0729 <connect0729@gmail.com>
2021-04-17 23:34:31 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CarDetailsComponent} from './car-details/car-details.component';
+import {PersonalDetailsComponent} from './personal-details/personal-details.component';
+import {CompletePageComponent} from './complete-page/complete-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,66 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {PersonalDetailsComponent} from './personal-details/personal-details.component';
+import {CarDetailsComponent} from './car-details/car-details.component';
+import {CompletePageComponent} from './complete-page/complete-page.component';
+import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {ToastrModule} from 'ngx-toastr';
+import {NgbDatepickerModule, NgbDropdownModule} from '@ng-bootstrap/ng-bootstrap';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgxSpinnerModule} from 'ngx-spinner';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalDetailsComponent,
+    CarDetailsComponent,
+    CompletePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: PersonalDetailsComponent,
+        data: {
+          title: 'personal details page'
+        }
+      },
+      {
+        path: 'carDetails',
+        component: CarDetailsComponent,
+        data: {
+          title: 'car details page'
+
+        }
+      },
+      {
+        path: 'completePage',
+        component: CompletePageComponent,
+        data: {
+          title: 'complete page'
+        }
+      }
+    ]),
+    FormsModule,
+    ToastrModule.forRoot({
+      timeOut: 5000,
+      positionClass: 'toast-bottom-right',
+      enableHtml: true
+    }),
+    NgbDatepickerModule,
+    NgbDropdownModule,
+    BrowserAnimationsModule,
+    NgxSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
